f3de4f9b63377eef4708e9fb67c9c733545b9923
Ying <milkar@gmail.com>
2010-04-12 22:55:54 +0200

    signup finished

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,7 +8,7 @@ describe UsersController do
     controller.should be_an_instance_of(UsersController)
   end
 
-
+  #for new user pages
   describe "GET 'new'" do
     
     before(:each) do
@@ -32,4 +32,60 @@ describe UsersController do
       response.should have_tag("h2", /#{@user.name}/)
     end
   end
+  
+  #for new/create user page after signup
+  describe "POST 'create'" do
+    
+    describe "failure" do
+      
+      before(:each) do
+        @attributes = { :name => "", :email => "", 
+                        :password => "", :password_confirmation => ""}
+        @user = Factory.build(:user, @attributes)
+        User.stub!(:new).and_return(@user)
+      end
+      
+      it "should not save the user" do
+        @user.should_receive(:save).and_return(false)
+        post :create, :user => @attributes 
+      end
+      
+      it "should have the right title" do
+        post :creat, :user => @attributes 
+        response.should render_template('new')
+      end
+      
+      it "should render the 'new' page" do
+        post :create, :user => @attributes
+        response.should render_template('new')
+      end    
+    end#for describe failure
+    
+    describe "success" do
+      
+      before(:each) do
+        @attributes = { :name => "New User", :email => "example@example.com",
+                        :password => "11111111", :password_confirmation => "11111111"}        
+        @user = Factory.build(:user, @attributes)
+        User.stub!(:new).and_return(@user)
+      end
+      
+      it "should save the new user" do
+        @user.should_receive(:save).and_return(true)
+        post :create, :user => @attributes 
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attributes 
+        respond.should redirect_to(user_url(@user))
+      end
+      
+      it "should have a welcome message" do
+        post :creat, :user => @attributes 
+        flash[:success].should =~ /Welcom/i   
+      end
+    end
+    
+  end
+  
 end
